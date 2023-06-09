c73d655e7edc487af90634461eb74cbfe44800ca
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-06-09 08:52:42 -0600

    Update find_pyarrow.py

--- ci-utils/find_pyarrow.py
+++ ci-utils/find_pyarrow.py
@@ -4,12 +4,14 @@ path=pa.get_library_dirs()[0] + '/libarrow_python'
 
 operating_system = platform.system()
 
+print(operating_system)
+
 file = ''
-if operating_system == 'linux':
+if operating_system == 'Linux':
     file = '.so'
-elif operating_system == 'darwin':
+elif operating_system == 'Darwin':
     file = '.dylib'
-elif operating_system == 'windows':
+elif operating_system == 'Windows':
     file = '.dll'
 
 path += file
