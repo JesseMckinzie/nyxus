85592ae47a1becd9d4b253b5233bbd5e7fdf05bc
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-06-30 10:55:04 -0600

    Update install_prereq_linux.sh

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -177,7 +177,7 @@ unzip arrow-apache-arrow-12.0.0.zip
 cd arrow-apache-arrow-12.0.0/cpp
 mkdir build
 cd build/
-cmake -DARROW_PARQUET="ON" -DARROW_DATASET="ON" .. 
+cmake -DARROW_PARQUET="ON" -DARROW_DATASET="ON" -DARROW_CSV="ON" .. 
 make install -j4
 cd ../../
 cd python/
