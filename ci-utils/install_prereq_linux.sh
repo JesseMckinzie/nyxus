d1a31831ceac5eaabc7a36f0e4f49f31202ca881
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-06-30 14:08:59 -0600

    Update install_prereq_linux.sh

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -175,23 +175,30 @@ cd ../../
 curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-12.0.0.zip -o  arrow-apache-arrow-12.0.0.zip
 unzip arrow-apache-arrow-12.0.0.zip
 cd arrow-apache-arrow-12.0.0/cpp
+mkdir dist
+export ARROW_HOME=$(pwd)/dist
+export LD_LIBRARY_PATH=$(pwd)/dist/lib:$LD_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=$ARROW_HOME:$CMAKE_PREFIX_PATH
 mkdir build
 cd build/
-cmake -DARROW_COMPUTE="ON" \
-    -DCMAKE_INSTALL_LIBDIR="lib" \
-    -DARROW_CSV="ON" \
-    -DARROW_DATASET="ON" \
-    -DARROW_FILESYSTEM="ON" \
-    -DARROW_HDFS="ON" \
-    -DARROW_JSON="ON" \
-    -DARROW_PARQUET="ON" \
-    -DARROW_WITH_BROTLI="ON" \
-    -DARROW_WITH_BZ2="ON" \
-    -DARROW_WITH_LZ4="ON" \
-    -DARROW_WITH_SNAPPY="ON" \
-    -DARROW_WITH_ZLIB="ON" \
-    -DARROW_WITH_ZSTD="ON" \
-    -DPARQUET_REQUIRE_ENCRYPTION="ON" \ 
+cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DARROW_BUILD_TESTS=ON \
+        -DARROW_COMPUTE=ON \
+        -DARROW_CSV=ON \
+        -DARROW_DATASET=ON \
+        -DARROW_FILESYSTEM=ON \
+        -DARROW_HDFS=ON \
+        -DARROW_JSON=ON \
+        -DARROW_PARQUET=ON \
+        -DARROW_WITH_BROTLI=ON \
+        -DARROW_WITH_BZ2=ON \
+        -DARROW_WITH_LZ4=ON \
+        -DARROW_WITH_SNAPPY=ON \
+        -DARROW_WITH_ZLIB=ON \
+        -DARROW_WITH_ZSTD=ON \
+        -DPARQUET_REQUIRE_ENCRYPTION=ON \
     .. 
 make -j4
 make install
