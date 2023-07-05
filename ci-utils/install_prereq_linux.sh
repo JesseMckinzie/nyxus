68513d41d794958157e9578f0bade012639606b0
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-07-05 09:49:05 -0600

    Update install_prereq_linux.sh

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -188,30 +188,13 @@ cd build/
 cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
         -DCMAKE_INSTALL_LIBDIR=lib \
         -DCMAKE_BUILD_TYPE=Release \
-        -DARROW_BUILD_TESTS=ON \
         -DARROW_COMPUTE=ON \
         -DARROW_CSV=ON \
         -DARROW_DATASET=ON \
-        -DARROW_FILESYSTEM=ON \
-        -DARROW_HDFS=ON \
-        -DARROW_JSON=ON \
-        -DARROW_PARQUET=ON \
-        -DARROW_WITH_BROTLI=ON \
-        -DARROW_WITH_BZ2=ON \
-        -DARROW_WITH_LZ4=ON \
-        -DARROW_WITH_SNAPPY=ON \
-        -DARROW_WITH_ZLIB=ON \
-        -DARROW_WITH_ZSTD=ON \
-        -DPARQUET_REQUIRE_ENCRYPTION=ON \
         -DARROW_ACERO=ON \
         .. 
 make -j4
 make install
 cd ../../
-cd python/
-export PYARROW_WITH_PARQUET=1
-export PYARROW_WITH_DATASET=1
-export PYARROW_PARALLEL=4
-python setup.py build_ext --inplace
-cd ../../
+
 
