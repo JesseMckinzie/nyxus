623b6eb36826bde9a38ce412bfa141b41fc503f8
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-01 20:53:30 -0600

    Update install_prereq_linux.sh

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -174,16 +174,6 @@ cd ../../
 
 ROOTDIR=$(pwd)
 
-
-curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
-unzip arrow-apache-arrow-13.0.0.zip
-cd arrow-apache-arrow-13.0.0
-cd cpp
-mkdir build
-cd build
-
-BUILDDIR=$(pwd)
-
 curl -L https://github.com/libevent/libevent/archive/refs/tags/release-2.1.12-stable.zip -o release-2.1.12-stable
 unzip release-2.1.12-stable
 cd libevent-release-2.1.12-stable
@@ -194,6 +184,20 @@ make
 make install
 cd ../../
 
+git clone https://github.com/apache/thrift.git
+cd thrift
+./bootstrap.sh
+./configure --with-lua=no
+make
+sudo make install
+cd ..
+
+curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
+unzip arrow-apache-arrow-13.0.0.zip
+cd arrow-apache-arrow-13.0.0
+cd cpp
+mkdir build
+cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=../../../$Z5_INSTALL_DIR \
         -DCMAKE_PREFIX_PATH=../../../$Z5_INSTALL_DIR \
