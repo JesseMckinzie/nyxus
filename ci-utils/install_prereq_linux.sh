1894efe011789f9bcdfabc8d8578d6ec7d5476ef
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-02 09:33:16 -0600

    Update install_prereq_linux.sh

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -174,16 +174,6 @@ cd ../../
 
 ROOTDIR=$(pwd)
 
-curl -L https://github.com/libevent/libevent/archive/refs/tags/release-2.1.12-stable.zip -o release-2.1.12-stable
-unzip release-2.1.12-stable
-cd libevent-release-2.1.12-stable
-mkdir build
-cd build
-cmake ..
-make
-make install
-cd ../../
-
 curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
 unzip arrow-apache-arrow-13.0.0.zip
 cd arrow-apache-arrow-13.0.0
