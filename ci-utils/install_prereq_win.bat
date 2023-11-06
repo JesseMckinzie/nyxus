f9ec8d6278c3fba4497a17f1c9926cce0ddd23f9
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-06 06:29:12 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -164,6 +164,10 @@ cmake --build . --config Release --target install --parallel 4
 popd
 popd
 
+set BOOST_INCLUDEDIR=local_install/boost_1_79_0
+set BOOST_LIBRARYDIR=local_install/boost_1_79_0/lib64-msvc-14.3
+SET BOOST_ROOT=local_install/boost_1_79_0/boost
+
 curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
 tar -xvf arrow-apache-arrow-13.0.0.zip
 pushd arrow-apache-arrow-13.0.0
