033b25c8bb52ae49201385191fd5e9dcd4cf703b
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-06 07:56:22 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -22,7 +22,7 @@ pushd boost_1_79_0
 call bootstrap.bat 
 .\b2 headers
 xcopy /E /I /y boost ..\local_install\include\boost
-.\b2 install --prefix=../../local_install/
+.\b2 install --prefix=../local_install/
 popd
 
 echo dir local_install
