e8eeb6c3955552d9ee5f15233a81dc9bf24d001e
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-05 21:52:35 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -170,7 +170,7 @@ pushd arrow-apache-arrow-13.0.0
 pushd cpp
 mkdir build
 pushd build
-cmake ..  -DCMAKE_INSTALL_PREFIX=../../../local_install/   -DCMAKE_PREFIX_PATH=../../../local_install/ -DBOOST_ROOT=../../../local_install/include/boost -DARROW_COMPUTE=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_ACERO=ON -DARROW_PARQUET=ON -DARROW_WITH_SNAPPY=ON 
+cmake ..  -DCMAKE_INSTALL_PREFIX=../../../local_install/   -DCMAKE_PREFIX_PATH=../../../local_install/ -DARROW_COMPUTE=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_ACERO=ON -DARROW_PARQUET=ON -DARROW_WITH_SNAPPY=ON 
 cmake --build . --config Release  --target install --parallel 4
 popd
 popd
