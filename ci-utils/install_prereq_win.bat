42ccb597cf539d838e3075ad9eafca8699f55dff
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-21 11:07:17 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -190,11 +190,6 @@ if "%BUILD_DCMTK_DEP%" == "1" (
     popd
     popd
 
-
-    vcpkg integrate install
-    vcpkg install arrow 
-
-    $env:CMAKE_TOOLCHAIN_FILE = "C:\vcpkg\scripts\buildsystems\vcpkg.cmake"
     )
 
 curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
