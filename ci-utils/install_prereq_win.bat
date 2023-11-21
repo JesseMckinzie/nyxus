2e35c7bacb16f7ae2dc2f615f1790fa4363a4061
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-21 13:33:08 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -192,17 +192,8 @@ if "%BUILD_DCMTK_DEP%" == "1" (
     popd
 
     )
-curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
-unzip arrow-apache-arrow-13.0.0.zip
-pushd arrow-apache-arrow-13.0.0
-pushd cpp
-mkdir build
-pushd build
-cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX=../../../local_install/   -DCMAKE_PREFIX_PATH=../../../local_install/ -DARROW_PARQUET=ON -DARROW_WITH_SNAPPY=ON
-cmake --build . --config Release
-popd 
-popd
-popd
+    
+pacman -S --noconfirm mingw-w64-x86_64-arrow
 
 if errorlevel 1 exit 1
 
