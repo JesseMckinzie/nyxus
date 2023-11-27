7bba1e7692ab29df16bb2ef2fbe3498a4e145215
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-11-27 07:36:35 -0700

    Update install_prereq_win.bat

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -116,6 +116,18 @@ if "%BUILD_Z5_DEP%" == "1" (
     popd
 )
 
+curl -L https://github.com/apache/arrow/archive/refs/tags/apache-arrow-13.0.0.zip -o  arrow-apache-arrow-13.0.0.zip
+unzip arrow-apache-arrow-13.0.0.zip
+pushd arrow-apache-arrow-13.0.0
+pushd cpp
+mkdir build
+pushd build
+cmake .. -G "MSYS Makefiles" -A x64 -DCMAKE_INSTALL_PREFIX=../../../local_install/ -DCMAKE_PREFIX_PATH=../../../local_install/ -DARROW_PARQUET=ON -DARROW_WITH_SNAPPY=ON
+make install
+popd 
+popd
+popd
+
 
 if "%BUILD_DCMTK_DEP%" == "1" (
     curl -L https://github.com/glennrp/libpng/archive/refs/tags/v1.6.39.zip -o v1.6.39.zip
