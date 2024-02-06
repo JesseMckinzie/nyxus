6958f890cf30fa13be154e942923af4adf64300c
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-06 14:31:14 -0700

    Update install_cuda_yum.sh

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -3,13 +3,18 @@
 # Defaults:
 #   $major_version = 12
 
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
+#yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
 yum clean all
+curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-nvcc-11-3-11.3.109-1.x86_64.rpm -o cuda-nvcc-11-3-11.3.109-1.x86_64.rpm
 
 
-echo "Installing CUDA version 11.3"
-yum -y install cuda-toolkit-11-3-11-3-1
+echo "Installing nvcc version 11.3"
+yum -y localinstall cuda-nvcc-11-3-11.3.109-1.x86_64.rpm
 
+curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-toolkit-11-3-11.3.1-1.x86_64.rpm -o cuda-toolkit-11-3-11.3.1-1.x86_64.rpm
+
+echo "Installing CUDA toolkit 11.3"
+yum -y localinstall cuda-toolkit-11-3-11.3.1-1.x86_64.rpm
 
 ls -al /usr/local
 export PATH=$PATH:/usr/local/cuda/bin
