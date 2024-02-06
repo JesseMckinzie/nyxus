737a4f8577e984f313b2d69ed02933533d2429c3
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-06 13:03:00 -0700

    Update install_cuda_yum.sh

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -3,11 +3,13 @@
 # Defaults:
 #   $major_version = 12
 
-#yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+
+yum list cuda-rhel7.repo
 
 yum clean all
 
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-11-3-11.3.1-1.x86_64.rpm -o cuda-11-3-11.3.1-1.x86_64.rpm
+#curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-11-3-11.3.1-1.x86_64.rpm -o cuda-11-3-11.3.1-1.x86_64.rpm
 
 echo "Installing CUDA version 11.3"
 yum -y localinstall cuda-11-3-11.3.1-1.x86_64.rpm
