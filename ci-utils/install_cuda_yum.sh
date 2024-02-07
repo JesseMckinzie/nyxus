d4505cf86367cb871f780cadd5581247da4471c5
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-07 07:36:21 -0700

    Update install_cuda_yum.sh

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -5,7 +5,15 @@
 
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
 yum clean all
-yum -y install cuda-toolkit-11-3-11.3.1-1
+
+if [[ "${major_version}" == "11" ]]; then
+    echo "Installing cuda toolkit 11"
+    yum -y install cuda-toolkit-11-3-11.3.1-1
+else
+    echo "Installing cuda toolkit 12"
+    yum -y install cuda-toolkit-12-3-12.3.1-1
+fi
+
 ls -al /usr/local
 export PATH=$PATH:/usr/local/cuda/bin
 nvcc --version
