6bed056a689e316df7a4513d3138c47454da6f88
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-07 07:26:50 -0700

    Update install_cuda_yum.sh

--- ci-utils/install_cuda_yum.sh
+++ ci-utils/install_cuda_yum.sh
@@ -3,77 +3,9 @@
 # Defaults:
 #   $major_version = 12
 
-#yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
-
-# install nvcc
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo &&
 yum clean all
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-nvcc-11-3-11.3.109-1.x86_64.rpm -o cuda-nvcc-11-3-11.3.109-1.x86_64.rpm
-
-
-echo "Installing nvcc version 11.3"
-yum -y localinstall cuda-nvcc-11-3-11.3.109-1.x86_64.rpm
-
-# install dependencies for cuda toolkit
-
-# Requires: cuda-samples-11-3 >= 11.3.58
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-samples-11-3-11.3.58-1.x86_64.rpm -o cuda-samples-11-3-11.3.58-1.x86_64.rpm
-echo "Installing cuda samples"
-yum -y localinstall cuda-samples-11-3-11.3.58-1.x86_64.rpm
-
-# Requires: cuda-tools-11-3 >= 11.3.1
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-tools-11-3-11.3.1-1.x86_64.rpm -o cuda-tools-11-3-11.3.1-1.x86_64.rpm
-echo "Installing cuda tools"
-yum -y localinstall cuda-tools-11-3-11.3.1-1.x86_64.rpm
-
-# Requires: cuda-documentation-11-3 >= 11.3.111
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-documentation-11-3-11.3.111-1.x86_64.rpm -o cuda-documentation-11-3-11.3.111-1.x86_64.rpm
-echo "Installing cuda documentation"
-yum -y localinstall cuda-documentation-11-3-11.3.111-1.x86_64.rpm
-
-
-# Requires: cuda-cuobjdump-11-3 >= 11.3.58
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-cuobjdump-11-3-11.3.58-1.x86_64.rpm -o cuda-cuobjdump-11-3-11.3.58-1.x86_64.rpm
-echo "Installing cuda cudobjdump"
-yum -y localinstall cuda-cuobjdump-11-3-11.3.58-1.x86_64.rpm
-
-# Requires: cuda-nvprune-11-3 >= 11.3.58
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-nvprune-11-3-11.3.58-1.x86_64.rpm -o ccuda-nvprune-11-3-11.3.58-1.x86_64.rpm
-echo "Installing cuda nvprune"
-yum -y localinstall cuda-nvprune-11-3-11.3.58-1.x86_64.rpm
-
-# Requires: cuda-cuxxfilt-11-3 >= 11.3.58
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-cuxxfilt-11-3-11.3.58-1.x86_64.rpm -o cuda-cuxxfilt-11-3-11.3.58-1.x86_64.rpm
-echo "Installing cuda cuxx"
-yum -y localinstall cuda-cuxxfilt-11-3-11.3.58-1.x86_64.rpm
- 
-
-
-
-# Requires: cuda-compiler-11-3 >= 11.3.1
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-compiler-11-3-11.3.1-1.x86_64.rpm -o cuda-compiler-11-3-11.3.1-1.x86_64.rpm
-echo "Installing cuda compiler"
-yum -y localinstall cuda-compiler-11-3-11.3.1-1.x86_64.rpm
-
-# Requires: cuda-libraries-11-3 >= 11.3.1
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-libraries-11-3-11.3.1-1.x86_64.rpm -o cuda-libraries-11-3-11.3.1-1.x86_64.rpm
-echo "Installing cuda libraries"
-yum -y localinstall cuda-libraries-11-3-11.3.1-1.x86_64.rpm
-
-# Requires: cuda-libraries-devel-11-3 >= 11.3.1
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-libraries-devel-11-3-11.3.1-1.x86_64.rpm -o cuda-libraries-devel-11-3-11.3.1-1.x86_64.rpm
-echo "Installing cuda libraries devel"
-yum -y localinstall cuda-libraries-devel-11-3-11.3.1-1.x86_64.rpm
-
-# Requires: cuda-nvml-devel-11-3 >= 11.3.58
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-nvml-devel-11-3-11.3.58-1.x86_64.rpm -o cuda-nvml-devel-11-3-11.3.58-1.x86_64.rpm
-echo "Installing cuda nvml"
-yum -y localinstall cuda-nvml-devel-11-3-11.3.58-1.x86_64.rpm
-
-# Install cuda toolkit
-curl -L https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-toolkit-11-3-11.3.1-1.x86_64.rpm -o cuda-toolkit-11-3-11.3.1-1.x86_64.rpm
-echo "Installing CUDA toolkit 11.3"
-yum -y localinstall cuda-toolkit-11-3-11.3.1-1.x86_64.rpm
-
+yum -y install cuda-toolkit-11-3-11.3.1-1
 ls -al /usr/local
 export PATH=$PATH:/usr/local/cuda/bin
 nvcc --version
