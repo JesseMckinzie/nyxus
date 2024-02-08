416f780ad0137a4a64d349c67327fdac3c74385b
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-08 14:35:54 -0700

    Update install_cuda_windows.bat

--- ci-utils/install_cuda_windows.bat
+++ ci-utils/install_cuda_windows.bat
@@ -1,6 +1,6 @@
 
 curl - L https://developer.download.nvidia.com/compute/cuda/11.3.1/network_installers/cuda_11.3.1_win10_network.exe -o cuda_11.3.1_win10_network.exe
 
-start cuda_11.3.1_win10_network.exe -s
+start cuda_11.3.1_win10_network.exe -s --accept-eula --no-window-prompt
 
 nvcc --version
