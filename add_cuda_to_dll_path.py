c26a910405027bb0ca44127b6d7c7e2e1888d090
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-22 11:01:30 -0700

    Add dll

--- add_cuda_to_dll_path.py
+++ add_cuda_to_dll_path.py
@@ -3,7 +3,12 @@ import os
 def add_cuda_to_path():
     if os.name != "nt":
         return
-    os.add_dll_directory("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin")
+    path = os.getenv("PATH")
+    if not path:
+        return 
+    path_split = path.split(";")
+    for folder in path_split:
+        if "cuda" in folder.lower() or "tensorrt" in folder.lower():
+            os.add_dll_directory(folder)
             
-
 add_cuda_to_path()
