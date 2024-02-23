56d3cae0b12d5090f69ba568b70fe71259747856
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-23 09:50:15 -0700

    Update dll search

--- add_cuda_to_dll_path.py
+++ add_cuda_to_dll_path.py
@@ -4,6 +4,5 @@ import shutil
 path = str(sys.executable)
 print(path)
 
-
 shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin\\cufft64_10.dll', path)
 shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin\\cudart64_110.dll', path)
