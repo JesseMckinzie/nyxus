6af51afcf8f2b8eea359e7edfeb5da39d0520637
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-23 09:13:21 -0700

    Update add_cuda_to_dll_path.py

--- add_cuda_to_dll_path.py
+++ add_cuda_to_dll_path.py
@@ -1,14 +1,7 @@
-import os
+from pathlib import Path
+import shutil
 
-def add_cuda_to_path():
-    if os.name != "nt":
-        return
-    path = os.getenv("PATH")
-    if not path:
-        return 
-    path_split = path.split(";")
-    for folder in path_split:
-        if "cuda" in folder.lower() or "tensorrt" in folder.lower():
-            os.add_dll_directory(folder)
-            
-add_cuda_to_path()
+path = str(Path(__file__).parent.resolve())
+
+shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin\\cufft64_10.dll', path)
+shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin\\cudart64_110.dll', path)
