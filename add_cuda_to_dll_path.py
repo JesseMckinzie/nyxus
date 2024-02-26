bffe264208b29bafc651982d04c0189ae6fc409d
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-26 09:16:30 -0700

    Update add_cuda_to_dll_path.py

--- add_cuda_to_dll_path.py
+++ add_cuda_to_dll_path.py
@@ -11,10 +11,5 @@ path = os.path.dirname(path)
 
 if not path.endswith('\\'):
     path += '\\'
-    
-print("the paths are: ")
-print(path + 'cufft64_10.dll')
-print(path + 'cudart64_110.dll')
 
-#shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin\\cufft64_10.dll', path + 'cufft64_10.dll')
-#shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin\\cudart64_110.dll', path + 'cudart64_110.dll')
+shutil.copyfile('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin', path)
