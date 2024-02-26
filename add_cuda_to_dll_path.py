20e36c49bcb87756cec81b3c05191f6bdf296561
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-26 14:15:37 -0700

    Update dll import

--- add_cuda_to_dll_path.py
+++ add_cuda_to_dll_path.py
@@ -1,16 +1,14 @@
-import sys
-from pathlib import Path
 import os
-import shutil
 
-path = path = str(Path(__file__).parent.resolve())
-
-print("the full path is: ")
-print(path)
-
-#path = os.path.dirname(path)
-
-if not path.endswith('\\'):
-    path += '\\'
-
-shutil.copytree('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8\\bin', path + 'tests\\python\\bin')
+def add_cuda_to_path():
+    if os.name != "nt":
+        return
+    path = os.getenv("PATH")
+    if not path:
+        return 
+    path_split = path.split(";")
+    for folder in path_split:
+        if "cuda" in folder.lower() or "tensorrt" in folder.lower():
+            os.add_dll_directory(folder)
+            
+add_cuda_to_path()
--- src/nyx/python/nyxus/__init__.py
+++ src/nyx/python/nyxus/__init__.py
@@ -5,3 +5,19 @@ from .functions import gpu_is_available, get_gpu_properties
 
 from . import _version
 __version__ = _version.get_versions()['version']
+
+import os
+
+def add_cuda_to_path():
+    if os.name != "nt" or not gpu_is_available():
+        return
+    path = os.getenv("PATH")
+    if not path:
+        return 
+    path_split = path.split(";")
+    for folder in path_split:
+        if "cuda" in folder.lower() or "tensorrt" in folder.lower():
+            os.add_dll_directory(folder)
+            
+add_cuda_to_path()
+
