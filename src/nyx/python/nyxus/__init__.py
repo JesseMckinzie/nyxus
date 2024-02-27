293544f439b0ec3713aa954a7f7bde8ec326b18e
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-26 17:20:35 -0700

    Update __init__.py

--- src/nyx/python/nyxus/__init__.py
+++ src/nyx/python/nyxus/__init__.py
@@ -1,16 +1,3 @@
-def add_cuda_to_path():
-    if os.name != "nt":
-        return
-    path = os.getenv("PATH")
-    if not path:
-        return 
-    path_split = path.split(";")
-    for folder in path_split:
-        if "cuda" in folder.lower() or "tensorrt" in folder.lower():
-            os.add_dll_directory(folder)
-            
-add_cuda_to_path()
-
 from .nyxus import Nyxus
 from .nyxus import Nyxus3D
 from .nyxus import Nested
